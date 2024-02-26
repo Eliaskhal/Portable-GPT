3fdc550d2f8023b0936e708012030c0b595a8e32
Ilyas Khallouki <khalloukielias@gmail.com>
2024-02-26 12:35:23 +0100

    Added a prompt file where the prompt will be and created an assistant object

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ load_dotenv()
 api_key = os.getenv("API_KEY")
 
 client = OpenAI(api_key=api_key)
+
+with open('prompt.txt', 'r') as f:
+    prompt = f.read()
+
+assistant = client.beta.assistants.create(
+    name="Terminal GPT",
+    instructions=prompt,
+    tools=[{"type": "code_interpreter"}],
+    model="gpt-4-turbo-preview"
+)
